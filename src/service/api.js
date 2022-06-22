b32ed318c6886a7769616a3f736ac11804572b13
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-22 16:00:31 +0300

    fetch movie details added. append to response reviews, credits added

--- src/service/api.js
+++ src/service/api.js
@@ -3,9 +3,18 @@ const API_KEY = 'e594f8b0e830f95dbd764645df4a6e75';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const TRENDING_URL = '/trending/movie/day';
+const MOVIES_URL = '/movie';
 
 export async function fetchTrendingMovies() {
   const response = await fetch(`${BASE_URL}${TRENDING_URL}?api_key=${API_KEY}`);
-  if (!response.ok) throw new Error('Response is not OK!');
+  if (!response.ok) throw new Error('Trending movies response is not OK!');
+  return response.json();
+}
+
+export async function fetchMovieDetails(movieId) {
+  const response = await fetch(
+    `${BASE_URL}${MOVIES_URL}/${movieId}?api_key=${API_KEY}&append_to_response=credits,reviews`
+  );
+  if (!response.ok) throw new Error('Movie details response is not OK!');
   return response.json();
 }
