e47a0f428adbc71802cff250665dd4cecaed3436
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-22 15:19:16 +0300

    trending movies fetched and viewed

--- src/components/Views/Home.jsx
+++ src/components/Views/Home.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchTrendingMovies } from 'service/api';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const [queryResult, setQueryResult] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const data = await fetchTrendingMovies();
+      //   console.log(data);
+      setQueryResult(data.results);
+    }
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <h2>Trending today</h2>
+      <ul>
+        {queryResult.map(({ title, id }) => (
+          <li key={id}>
+            <Link to={`movies/${id}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
