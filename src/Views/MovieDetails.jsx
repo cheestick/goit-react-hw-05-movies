38c20bbc5dbe9cb47fabb2e13aa266b5bcee028c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-24 14:11:10 +0300

    back button added and fixed

--- src/Views/MovieDetails.jsx
+++ src/Views/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useParams } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { fetchMovieDetails } from 'service/api';
 import MovieCard from 'components/MovieCard';
@@ -8,6 +8,8 @@ import s from './MovieDetails.module.css';
 export function MovieDetails() {
   const { movieId } = useParams();
   const [movieInfo, setMovieInfo] = useState({});
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchMovieInfo() {
@@ -19,14 +21,29 @@ export function MovieDetails() {
 
   return (
     <div className={s.wrapper}>
+      <button
+        className={s.backbutton}
+        type="button"
+        onClick={() => navigate(location.state?.from || '/')}
+      >
+        Go back
+      </button>
       <MovieCard movieInfo={movieInfo} />
       <div className={s.additional}>
         <h2>Additional information</h2>
         <nav className={s.extraNavigation}>
-          <NavLink className={s.extralink} to="cast">
+          <NavLink
+            className={s.extralink}
+            to="cast"
+            state={location.state && { from: location.state.from }}
+          >
             Cast
           </NavLink>
-          <NavLink className={s.extralink} to="review">
+          <NavLink
+            className={s.extralink}
+            to="review"
+            state={location.state && { from: location.state.from }}
+          >
             Review
           </NavLink>
         </nav>
--- src/components/MovieSearchForm/MovieSearchForm.jsx
+++ src/components/MovieSearchForm/MovieSearchForm.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useLocation, useSearchParams, Link } from 'react-router-dom';
 import { fetchMovieByName } from 'service/api';
 import s from './MovieSearchForm.module.css';
 
 export default function MovieSearchForm() {
   const [query, setQuery] = useSearchParams();
-  const [searchRequest, setSearchReaquest] = useState(
-    () => query.get('query') ?? ''
-  );
-
+  const [searchRequest, setSearchReaquest] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
   const location = useLocation();
-  console.log(location);
 
-  async function onSubmit(e) {
+  useEffect(() => {
+    async function fetchMovie(movieTitle) {
+      const res = await fetchMovieByName(movieTitle);
+      setSearchResult(res.results);
+    }
+    const prevQuery = query.get('query');
+    if (!prevQuery) return;
+    fetchMovie(prevQuery);
+  }, [query]);
+
+  function onSubmit(e) {
     e.preventDefault();
-    console.log(query.toString());
+    if (!searchRequest) return;
     setQuery({ query: searchRequest });
-    const res = await fetchMovieByName(searchRequest);
-    console.log(res);
     setSearchReaquest('');
   }
 
   return (
-    <form className={s.form} onSubmit={onSubmit} autoComplete="off">
-      <input
-        type="text"
-        name="search"
-        value={searchRequest}
-        onChange={({ target }) => setSearchReaquest(target.value)}
-      />
-      <button type="submit">Search</button>
-    </form>
+    <>
+      <form className={s.form} onSubmit={onSubmit} autoComplete="off">
+        <input
+          type="text"
+          name="search"
+          value={searchRequest}
+          onChange={({ target }) => setSearchReaquest(target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {searchResult.length > 0 && (
+        <ul>
+          {searchResult.map(({ title, id }) => (
+            <li key={id}>
+              <Link to={`${id}`} state={{ from: location }}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
